0f95991d22864033251adb9611563bce0adbef8d
tahanimadmad <43748894+tahanimadmad@users.noreply.github.com>
2021-03-23 16:18:51 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,178 @@
 
+# Imports
+import os
+from os.path import isfile, join
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import shutil
+from random import randint
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torch.autograd import Variable
+from tqdm import tqdm
+import random
+from torchvision.utils import save_image
+import sys
+import torchvision.transforms.functional as TTF
+from math import exp
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.optim import Adam
+from datetime import datetime
+import torch_optimizer as toptim
+
+# UTILS
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print('EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print('Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
+
+# Definition of a class to log values during training
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+
+def create_window(window_size, sigma, channel):
+    _1D_window = gaussian(window_size, sigma).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
+
+
+class MS_SSIM(torch.nn.Module):
+    def __init__(self, size_average=True, max_val=255, window_size=7):
+        super(MS_SSIM, self).__init__()
+        self.size_average = size_average
+        self.channel = 1
+        self.max_val = max_val
+        self.window_size = window_size
+
+    def _ssim(self, img1, img2, size_average=True, window_size=7):
+
+        _, c, w, h = img1.size()
+        sigma = 1.5 * window_size / 11
+        window = create_window(window_size, sigma, self.channel).cuda()
+        mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=self.channel)
+        mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=self.channel)
+
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+        mu1_mu2 = mu1 * mu2
+
+        sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=self.channel) - mu1_sq
+        sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=self.channel) - mu2_sq
+        sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=self.channel) - mu1_mu2
+
+        C1 = (0.01 * self.max_val) ** 2
+        C2 = (0.03 * self.max_val) ** 2
+        V1 = 2.0 * sigma12 + C2
+        V2 = sigma1_sq + sigma2_sq + C2
+        ssim_map = ((2 * mu1_mu2 + C1) * V1) / ((mu1_sq + mu2_sq + C1) * V2)
+        mcs_map = V1 / V2
+        if size_average:
+            return ssim_map.mean(), mcs_map.mean()
+
+    def ms_ssim(self, img1, img2, levels=3):
+
+        weight = Variable(torch.Tensor([0.0448, 0.2856, 0.3001]).cuda())  # , 0.2363, 0.1333]
+        msssim = Variable(torch.Tensor(levels, ).cuda())
+        mcs = Variable(torch.Tensor(levels, ).cuda())
+        for i in range(levels):
+            ssim_map, mcs_map = self._ssim(img1, img2)
+            msssim[i] = ssim_map
+            mcs[i] = mcs_map
+            filtered_im1 = F.avg_pool2d(img1, kernel_size=2, stride=2)
+            filtered_im2 = F.avg_pool2d(img2, kernel_size=2, stride=2)
+            img1 = filtered_im1
+            img2 = filtered_im2
+
+        value = (torch.prod(mcs[0:levels - 1] ** weight[0:levels - 1]) *
+                 (msssim[levels - 1] ** weight[levels - 1]))
+        return value
+
+    def forward(self, img1, img2):
+
+        return self.ms_ssim(img1, img2)
+      
+      
+# A function to change a tensor to numpy
+def to_np(x):
+    return x.data.cpu().numpy()
+
+
+# A function to return the learning rate parameter
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
